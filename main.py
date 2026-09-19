e693afa71c8064f4ed150571a63bd3ffa0bf5af7
Oleg Markin <o51393680@gmail.com>
2026-09-19 10:54:40 +0300

    На тестовой ветке разработал полностью систему вывода и поиска путешействий

--- main.py
+++ main.py
@@ -26,8 +26,23 @@ def Main(missions_list):
     for always_int, moved_missions_list in enumerate(missions_list, start=1):
         print(f"{always_int}._________________________________\n   |- Название: {moved_missions_list['name']} -|\n   |- Год: {moved_missions_list['year']} -|\n   |- Направление: {moved_missions_list['direction']} -|\n   |_______________________________\n")
         xdwd(0.3)
+    print(f"\nВведите направление для поиска: ", end='')
+    dire = input()
 
+    is_any_find = 0
 
+    risults = []
+    for poisk in missions_list:
+        if poisk["direction"].lower() == dire.lower():
+            risults.append(poisk)
+            is_any_find += 1
+    if is_any_find == 0:
+        print("Не найдено ни одной миссии ;(! Сожалеем!")
+    elif is_any_find != 0:
+        print(f"По вашему запросу найдено {is_any_find} результатов.\nВот эти миссии:\n")
+        for always_int1, moved_risults in enumerate(risults, start=1):
+            print(f"{always_int1}._________________________________\n   |- Название: {moved_risults['name']} -|\n   |- Год: {moved_risults['year']} -|\n   |- Направление: {moved_risults['direction']} -|\n   |_______________________________")
+    
 
 if __name__ == "__main__":
     Main(missions_list)
